71404fa20bee5c95357f09efff882f7e793072b2
careplushealth <info@careplushealth.co.uk>
2026-01-17 04:37:05 +0000

    Change base path in Vite config to match casing

--- careplus-leave-tracker/vite.config.js
+++ careplus-leave-tracker/vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  base: "/holiday-tracker/",
   plugins: [react()],
+  base: "/Holiday-Tracker/",
 });
